583eae9035c34797e35c9ba080d06a2312bb22c6
Khuziama <rehmankhuziama@gmail.com>
2021-07-02 16:33:37 -0400

    css for sorting array ( need to fix margins)

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,8 +1,7 @@
-import { func } from 'prop-types';
 import React from 'react';
 import './SortingVisualizer.css';
 
-class SortingVisualizer extends React.Component {
+export default class SortingVisualizer extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -12,28 +11,25 @@ class SortingVisualizer extends React.Component {
 	componentDidMount() {
 		this.resetArray();
 	}
+	randomNumFromInterval(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
 	resetArray() {
 		const array = [];
-		for (let i = 0; i < 100; i++) {
-			array.push(randomNumFromInterval(5, 1000));
+		for (let i = 0; i < 310; i++) {
+			array.push(this.randomNumFromInterval(5, 1000));
 		}
+
 		this.setState({ array });
 	}
 
 	render() {
 		const { array } = this.state;
-        return (
-            <>
-            {array.map((value,idx) => {
-                <div className="array-bar" key={idx}>
-                {value}
-                </div>
-            })}
-            </>
-        )
+
+		return (
+			<div className="array-container">
+				{array.map((value, idx) => <div className="array-bar" key={idx} style={{ height: `${value}px` }} />)}
+			</div>
+		);
 	}
 }
-function randomNumFromInterval(min,max){
-     return Math.random() * (max - min) + min;
-}
-export default SortingVisualizer;
